81a63c631501952e97751ea476b925fa88feb70a
Roger Dungan <mustangrog@hotmail.com>
2019-07-09 20:47:56 -0500

    Ask user for name, creat variable in memory

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Create a greeter function that will use the persons name and display "Hello" and their name in <h1>
+
+/*
+  3. Construct or Concatenate a string with 'hello' and the value stored in memory for their name.
+  4. Select the <h1> tag on the page.
+  5. Replace the value of the <h1> tag.
+*/
+
+const name = prompt('What is your name?');
