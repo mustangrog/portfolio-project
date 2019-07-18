aae0ec97908b865f1eff9dd473ebc316e1436af5
Roger Dungan <mustangrog@hotmail.com>
2019-07-17 20:56:57 -0500

    Re-render the page with the correct title based on a click event

--- index.js
+++ index.js
@@ -3,15 +3,53 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-const state = {
-    'title': 'This is the page Title!'
+const states = {
+    'home': {
+        'title': 'This is the page Title!'
+    },
+    'about': {
+        'title': 'About Page'
+    },
+    'contact': {
+        'title': 'Contact'
+    }
 };
 
-document.querySelector('#root').innerHTML = `
-${Navigation()}
-${Header(state)}
-${Main()}
-${Footer()}
-`;
+function render(state){
+    document.querySelector('#root').innerHTML = `
+  ${Navigation(state)}
+  ${Header(state)}
+  ${Main(state)}
+  ${Footer(state)}
+  `;
+}
 
-console.log(Navigation, Header, Main, Footer);
+render(states.home);
+// The elements will not exist until page is rendered.  This is placed after the render item.
+const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
+
+let i = 0;
+// TODO: Refactor this loop structure
+// Use the value of i as an index to access the nav item.
+// Here I will run until 0-3, once it hits 3 the while statement stops.
+
+while(i < navItems.length){
+    // Ass this callback function to each of the navItems.
+    navItems[i].addEventListener('click', function clickHandler(event){
+        event.preventDefault();
+        // 'target' will reveal what generated the event - that is, what was 'clicked'.
+        const clickedItem = event.target.textContent;
+        const clicked = clickedItem.toLowerCase();
+
+        render(states[clicked]);
+    });
+    i++;
+}
+
+// We want to re-render our page based on what the user clicks in our nav menu.
+/*
+1.Capture what was clicked in navigation - what is the text of the element that was clicked?
+2.Access a piece of state based on said text.
+3.Show the user the new view.
+*/
+
