14fe696f4688bd36286be8dcb87cf451974a5e77
Roger Dungan <mustangrog@hotmail.com>
2019-07-29 19:01:56 -0500

    add npm scripts

--- index.js
+++ index.js
@@ -38,31 +38,3 @@ navItems.forEach(function eventListenerAdder(navItem){
         render(states[event.target.textContent.toLowerCase()]);
     });
 });
-
-// TODO: Refactor this loop structure
-// Use the value of i as an index to access the nav item.
-// Here I will run until 0-3, once it hits 3 the while statement stops.
-
-// while(i < navItems.length){
-//     // Add this callback function to each of the navItems.
-//     navItems[i].addEventListener('click', function clickHandler(event){
-//         event.preventDefault();
-//         // 'target' will reveal what generated the event - that is, what was 'clicked'.
-//         const clickedItem = event.target.textContent;
-//         const clicked = clickedItem.toLowerCase();
-
-//         // TODO: [textContent.toLowerCase()];
-
-
-//         render(states[clicked]);
-//     });
-//     i++;
-// }
-
-// We want to re-render our page based on what the user clicks in our nav menu.
-/*
-1.Capture what was clicked in navigation - what is the text of the element that was clicked?
-2.Access a piece of state based on said text.
-3.Show the user the new view.
-*/
-
