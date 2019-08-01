47b831c9fa3c69ab0c7db614a33e9059b36f538f
Roger Dungan <mustangrog@hotmail.com>
2019-07-31 20:44:57 -0500

    add state to page

--- components/Main.js
+++ components/Main.js
@@ -1,40 +1,7 @@
 export default function(state){
     return `
 <main>
-<!-- Sections are like subtopics directly related to the general topic of the page -->
-<section>
-  <h2>This Is Section 1</h2>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquam
-    aliquid exercitationem vitae in, eius odit, itaque eligendi, hic
-    facere ad veniam necessitatibus dolore nemo natus modi? Voluptatibus,
-    sit similique!
-  </p>
-  <!--CTA=Call To Action-->
-  <a href="#" class="cta-btn">Read More!</a>
-</section>
-
-<section>
-  <h2>This Is Section 2</h2>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquam
-    aliquid exercitationem vitae in, eius odit, itaque eligendi, hic
-    facere ad veniam necessitatibus dolore nemo natus modi? Voluptatibus,
-    sit similique!
-  </p>
-  <a href="#" class="cta-btn">Read More!</a>
-</section>
-
-<section>
-  <h2>This Is Section 3</h2>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquam
-    aliquid exercitationem vitae in, eius odit, itaque eligendi, hic
-    facere ad veniam necessitatibus dolore nemo natus modi? Voluptatibus,
-    sit similique!
-  </p>
-  <a href="#" class="cta-btn">Read More!</a>
-</section>
+${state.page}
 </main>
 `
     ;
