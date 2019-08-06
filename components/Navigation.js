1bfba4ca6327d4aa4af910642140a80a200622c5
Roger Dungan <mustangrog@hotmail.com>
2019-08-06 13:19:03 -0500

    Use Navigo to simplify'click' navigation code

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,10 @@
 function linkBuilder(links){
     return links.map(function linkLister(link){
-        return `<li>${link}</li>`;
+        /**
+         * An HTML data attribute provides additional information that can be used as a hook.
+         * In this case, 'navigo' is something that navigo uses to bind the link 'text' to the Navigo router.
+         */
+        return `<li><a href="/${link.toLowerCase()}" data-navigo>${link}</a></li>`;
     }).join(' ');
 }
 
